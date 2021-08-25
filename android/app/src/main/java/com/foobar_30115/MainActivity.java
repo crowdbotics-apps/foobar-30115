a91229a0ccf6b19496c494fc85f51273316b2af8
Crowdbotics <team@crowdbotics.com>
2021-08-25 23:30:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/foobar_30115/MainActivity.java
@@ -0,0 +1,15 @@
+package com.foobar_30115;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "foobar_30115";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "foobar_30115Identifier",
+  url: "https://foobar_30115Identifier.botics.co",
+  api: "https://foobar_30115Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
